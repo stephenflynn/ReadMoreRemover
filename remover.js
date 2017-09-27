dffb6479da92f615b0f7e97e49bb4b4847b93932
Stephen Flynn <dev@stephenflynn.net>
2017-09-27 13:38:34 -0700

    Initial commit

---
+++ remover.js
@@ -0,0 +1,5 @@
+  document.addEventListener("DOMContentLoaded", function(event) {
+    var read_more = document.querySelector('.read-more-link');
+    console.info('checking this works !!!!')
+    read_more.click()
+  });
