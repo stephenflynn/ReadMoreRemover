1bf20dd8cda60d63507993d8c03ba658e0354bab
Stephen Flynn <dev@stephenflynn.net>
2017-11-07 14:41:10 -0800

    Added support for more sites and added a bug fix for alo.com

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # ReadMoreRemover
 
-#### A simple extension for **Firefox for Android** that automatically clicks the Continue Reading button on [http://www.huffingtonpost.com](huffingtonpost.com)
+#### A simple extension for **Firefox for Android** that automatically clicks the Continue Reading button on:
+
+- [http://www.aol.com](aol.com)
+- [http://www.huffingtonpost.com](huffingtonpost.com)
+- [http://www.thejournal.ie](thejournal.ie)
+- [http://www.inquisitr.com](inquisitr.com)
+- [http://qz.com](qz.com)
 
 I created this add on because I do not like having to click a button to reveal more of an article having navigated to it in the first place. If you have any suggestions for other sites that have Read More/Continue Reading buttons create and issue and I'll have a look into adding support for them.
 
--- remover.js
+++ remover.js
@@ -1,16 +1,30 @@
 var currentLocation = window.location;
 var selector_name = '';
 
-switch (currentLocation.host) {
-    case "m.huffpost.com":
-    case "huffpost.com":    
-    case "huffingtonpost.co.uk":
-    case "huffingtonpost.com":
-        selector_name = '.read-more-link';
-    break;
+switch (currentLocation.hostname) {
+    case "www.aol.com":
     case "aol.com":
         selector_name = '.article-show-more';
+    break; 
+    case "m.huffpost.com":
+    case "www.huffpost.com":    
+    case "www.huffingtonpost.co.uk":
+    case "www.huffingtonpost.com":
+    case "huffpost.com":    
+    case "huffingtonpost.co.uk":
+    case "huffingtonpost.com":    
+        selector_name = '.read-more-link';
     break;
+    case "www.inquisitr.com":
+        selector_name = '.inq-article-readmore--btn';
+    break;
+    case "www.qz.com":
+    case "qz.com":
+        selector_name = '.read-full-story-button';
+    break;   
+    case "www.thejournal.ie":
+        selector_name = '#readmore';
+    break;     
 }
 
 if (selector_name) {
